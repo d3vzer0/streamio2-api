84b53394e73ca32832c905b9b34c52e1fb76794e
Joey Dreijer <jdreijer@schubergphilis.com>
2021-02-07 17:01:01 +0100

    Implemented native multisearch

--- app/routers/v1/search/api.py
+++ app/routers/v1/search/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from elasticsearch import Elasticsearch
 from app.environment import config
 from .schema import SearchOut, SearchIn, HistogramIn
-from .utils import AbstractSearch
+from .utils import AbstractSearch, AbstractMultiSearch
 from typing import List
 from ssl import create_default_context
 import ssl
@@ -14,34 +14,21 @@ router = APIRouter()
 @router.post('/search', response_model=List[SearchOut])
 async def search_es(searches: List[SearchIn]):
     search_objects = [search.dict(exclude_none=True) for search in searches]
-    async with AbstractSearch(es_client=es_client, index=config['index_pattern']) as abstract_search:
-        results = [
-            {
-                **await abstract_search.get(query=search['query'],
-                    filters=search['filters'],
-                    start=search['start'],
-                    limit=search['limit'],
-                    from_date=search['from_date'],
-                    to_date=search['to_date'],
-                    aggregations=search['aggregations'],
-                    exclude=search['exclude']),
-                'column':search['column']
-            }
-            for search in search_objects
-        ]
+    async with AbstractMultiSearch(es_client=es_client, index=config['index_pattern']) as abstract_search:
+        results = await abstract_search.multisearch(search_objects)
         return results
 
-@router.post('/histogram')
-async def histogram_es(search: HistogramIn):
-    search_object = search.dict(exclude_none=True)
-    async with AbstractSearch(es_client=es_client, index=config['index_pattern']) as abstract_search:
-        results = await abstract_search.get_hist(query=search_object['query'],
-            filters=search_object['filters'],
-            field=search_object['field'],
-            from_date=search_object['from_date'],
-            to_date=search_object['to_date'],
-            interval=search_object['interval'],
-            exclude=search_object['exclude']
-        )
-        return results
+# @router.post('/histogram')
+# async def histogram_es(search: HistogramIn):
+#     search_object = search.dict(exclude_none=True)
+#     async with AbstractSearch(es_client=es_client, index=config['index_pattern']) as abstract_search:
+#         results = await abstract_search.get_hist(query=search_object['query'],
+#             filters=search_object['filters'],
+#             field=search_object['field'],
+#             from_date=search_object['from_date'],
+#             to_date=search_object['to_date'],
+#             interval=search_object['interval'],
+#             exclude=search_object['exclude']
+#         )
+#         return results
     
--- app/routers/v1/search/utils.py
+++ app/routers/v1/search/utils.py
@@ -1,10 +1,11 @@
-from elasticsearch_dsl import Search, A
+from elasticsearch_dsl import Search, MultiSearch, A
 
 class AbstractSearch:
     def __init__(self, es_client=None, index=None):
         self.es_client = es_client
         self.index = index
         self.search = None
+        self.multi_search = None
 
     @staticmethod
     def es2api(response, start, limit):
@@ -17,10 +18,12 @@ class AbstractSearch:
             'limit': limit
         }
 
+
     async def get(self, query='*', filters=None, aggregations=None, exclude=None, 
         to_date=None, from_date=None, start=0, limit=30):
         self.search = Search(using=self.es_client, index=self.index)
         self.__filters(filters, to_date, from_date)
+        # print(self.search.to_dict())
         self.__exclude(exclude)
         self.__query(query)
         self.__aggregations(aggregations)
@@ -54,7 +57,6 @@ class AbstractSearch:
 
     def __aggregations(self, aggregations):
         for key, value in aggregations.items():
-            print(key, value)
             try:
                 aggregation = A(value['type'], field=f'{key}.keyword')
                 self.search.aggs.bucket(key, aggregation)
@@ -76,6 +78,94 @@ class AbstractSearch:
     
     async def __aenter__(self):
         self.search = Search(using=self.es_client, index=self.index)
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        return self
+
+
+
+class AbstractMultiSearch:
+    def __init__(self, es_client=None, index=None, multi_search=None):
+        self.es_client = es_client
+        self.index = index
+        self.multi_search = multi_search
+
+    @staticmethod
+    def es2api(response, start, limit, column=None):
+        return {
+            'total': response['hits']['total']['value'],
+            'results': response['hits']['hits'],
+            'aggregations': {key: value['buckets'] for key, value \
+                in response.get('aggregations', {}).items()},
+            'start': start,
+            'limit': limit,
+            'column': column
+        }
+
+    async def multisearch(self, searches):
+        for search in searches:
+            search_object = self.__get(
+                query=search['query'],
+                filters=search['filters'],
+                start=search['start'],
+                limit=search['limit'],
+                from_date=search['from_date'],
+                to_date=search['to_date'],
+                aggregations=search['aggregations'],
+                exclude=search['exclude']
+            )
+            self.multi_search = self.multi_search.add(search_object)
+        try:
+            responses = [
+                AbstractMultiSearch.es2api(
+                    response.to_dict(),
+                    searches[index]['start'],
+                    searches[index]['limit'],
+                    column=searches[index]['column']) for \
+                    (index, response) in enumerate(self.multi_search.execute())
+                ]
+            return responses
+
+        except Exception as err:
+            print(err)
+
+    def __get(self, query='*', filters=None, aggregations=None, exclude=None, 
+        to_date=None, from_date=None, start=0, limit=30):
+        single_search = Search(using=self.es_client, index=self.index)
+        single_search = self.__filters(single_search, filters, to_date, from_date)
+        single_search = self.__exclude(single_search, exclude)
+        single_search = self.__query(single_search, query)
+        single_search = self.__aggregations(single_search, aggregations)
+        single_search = single_search.sort('-publish_date')[start:start+limit]
+        return single_search
+  
+    def __aggregations(self, search, aggregations):
+        for key, value in aggregations.items():
+            try:
+                aggregation = A(value['type'], field=f'{key}.keyword')
+                search.aggs.bucket(key, aggregation)
+            except Exception as err:
+                print(err)
+        return search
+    
+    def __filters(self, search, filters, to_date, from_date):
+        search = search.filter('range', **{f'publish_date': {'gte':from_date ,'lte':to_date}})
+        for key, value in filters.items():
+           search = search.filter(value['type'], **{f'{key}.keyword': value[value['type']]})
+        return search
+
+    def __exclude(self, search, exclude):
+        for value in exclude:
+            search = search.exclude(value['type'], **{f'{value["field"]}.keyword': value[value['type']]})
+        return search
+
+    def __query(self, search, query):
+        search = search.query('simple_query_string', query=query)
+        return search
+    
+    async def __aenter__(self):
+        self.multi_search = MultiSearch(using=self.es_client, index=self.index)
         return self
 
     async def __aexit__(self, *args, **kwargs):
