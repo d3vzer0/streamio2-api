77352e280b87170a6a0a3e13cf21b8fb4aae7d35
Joey Dreijer <jdreijer@schubergphilis.com>
2021-02-07 17:01:55 +0100

    Removed histogram search for now

--- app/routers/v1/search/api.py
+++ app/routers/v1/search/api.py
@@ -17,18 +17,3 @@ async def search_es(searches: List[SearchIn]):
     async with AbstractMultiSearch(es_client=es_client, index=config['index_pattern']) as abstract_search:
         results = await abstract_search.multisearch(search_objects)
         return results
-
-# @router.post('/histogram')
-# async def histogram_es(search: HistogramIn):
-#     search_object = search.dict(exclude_none=True)
-#     async with AbstractSearch(es_client=es_client, index=config['index_pattern']) as abstract_search:
-#         results = await abstract_search.get_hist(query=search_object['query'],
-#             filters=search_object['filters'],
-#             field=search_object['field'],
-#             from_date=search_object['from_date'],
-#             to_date=search_object['to_date'],
-#             interval=search_object['interval'],
-#             exclude=search_object['exclude']
-#         )
-#         return results
-    
